8900da89be1dc87358653d6cceafd168ec4564bb
junioramaral22 <junior.amaral.1818@gmail.com>
2025-02-11 13:56:36 -0300

    feat: add user

--- back/study-red/src/app.service.ts
+++ back/study-red/src/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const vtnc = process.env.FIREBASE_ADMIN_CREDENTIALS
+    console.log(vtnc);
+    return 'hello';
   }
 }
