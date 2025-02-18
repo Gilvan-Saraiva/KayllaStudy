ba96ec81dffa6aa2d34f8a776046cd65a0a42aa4
Gilvan-Saraiva <juniorsaraivabi@gmail.com>
2025-02-18 16:17:19 -0300

    feat: carousel

--- front/study-red/src/app/home/home.component.ts
+++ front/study-red/src/app/home/home.component.ts
@@ -1,14 +1,26 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  standalone: true
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  constructor(private ngZone: NgZone) {}
 
-
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.ngZone.runOutsideAngular(() => {
+        const elements = document.querySelectorAll('.collapsible-caption');
+        elements.forEach((element) => {
+          element.addEventListener('click', function (this: HTMLElement) {
+            this.classList.toggle('expanded');
+          });
+        });
+      });
+    }, 100);
+  }
 }
