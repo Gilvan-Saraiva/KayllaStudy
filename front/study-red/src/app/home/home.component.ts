b03e389b15d0d92105ac91c02e563859accf2efa
junioramaral22 <junior.amaral.1818@gmail.com>
2025-03-05 12:52:08 -0300

    estilização home

--- front/study-red/src/app/home/home.component.ts
+++ front/study-red/src/app/home/home.component.ts
@@ -1,28 +1,38 @@
 import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { Component, AfterViewInit, NgZone, PLATFORM_ID, Inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements AfterViewInit {
-  constructor(private ngZone: NgZone, @Inject(PLATFORM_ID) private platformId: any) {}
-
+  constructor(private ngZone: NgZone, @Inject(PLATFORM_ID) private platformId: any, private router: Router) {}
+  redirectToRegister() {
+  console.log('Botão clicado!'); // Verifique se essa mensagem aparece no console
+  this.router.navigate(['/register']);
+}
   ngAfterViewInit() {
     if (isPlatformBrowser(this.platformId)) {
-      setTimeout(() => {
-        this.ngZone.runOutsideAngular(() => {
-          const elements = document.querySelectorAll('.collapsible-caption');
-          elements.forEach((element) => {
-            element.addEventListener('click', function (this: HTMLElement) {
-              this.classList.toggle('expanded');
-            });
+      this.ngZone.runOutsideAngular(() => {
+        const captions = document.querySelectorAll('.carousel-caption');
+        captions.forEach(caption => {
+          caption.addEventListener('click', () => {
+            const text = caption.querySelector('.truncate-text') as HTMLElement | null;
+            const readMore = caption.querySelector('.read-more') as HTMLElement | null;
+            const readLess = caption.querySelector('.read-less') as HTMLElement | null;
+
+            if (text && readMore && readLess) {
+              text.classList.toggle('expanded');
+              readMore.style.display = text.classList.contains('expanded') ? 'none' : 'block';
+              readLess.style.display = text.classList.contains('expanded') ? 'block' : 'none';
+            }
           });
         });
-      }, 100);
+      });
     }
   }
 }
